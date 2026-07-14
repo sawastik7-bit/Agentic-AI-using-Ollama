2cf57eee24e7634362aecba9392c146942d9e16d
Sawastik Bhullar <sawastik7@gmail.com>
2026-07-14 21:48:18 +0530

    added the getweather function and tested it

--- src/GetWeatherTool.js
+++ src/GetWeatherTool.js
@@ -1,4 +1,23 @@
 
+import GetLocation from "./Functions/GetLocation.js"
+
+
+
+const getWeather=async(city)=>{
+
+    const {latitude,longitude}=await GetLocation(city);
+
+    const weatherRes = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weather_code`
+  );
+
+    let weatherData=await weatherRes.json();
+
+    console.log(weatherData);
+    
+}
+
+getWeather("kharar");
 
 
 // obj.weather naam ka array usme obj.main mein current weather and obj.description me moderate rain
